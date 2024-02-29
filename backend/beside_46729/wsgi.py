e77311c419c9eee2ea51b9cc370dadb4820b8895
Crowdbotics <team@crowdbotics.com>
2024-02-29 21:30:16 +0000

    Initial commit

---
+++ backend/beside_46729/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for beside_46729 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "beside_46729.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "beside-46729",
+  url: "https://beside-46729.botics.co",
+  api: "https://beside-46729.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
